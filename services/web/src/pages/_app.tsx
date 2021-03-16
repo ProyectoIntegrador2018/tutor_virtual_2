0adf687032220aaef5550d2c0abdfa90cc94ae79
Hector <hectordelunap@gmail.com>
2021-03-16 17:37:42 -0600

    Provide auth a _app

--- services/web/src/pages/_app.tsx
+++ services/web/src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ProvideAuth } from "../lib/hooks/useAuth";
 import { theme } from "../theme";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ProvideAuth>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ProvideAuth>
   );
 }
 
