aa6463dd7f3ab663a2ca2f40339e0e2f25415210
hectordeluna <hectordelunap@gmail.com>
2021-03-25 16:49:40 -0600

    Navbar Layout and navbar mobile

--- services/web/src/components/modules/Navbar/index.tsx
+++ services/web/src/components/modules/Navbar/index.tsx
@@ -11,27 +11,29 @@ export default function Navbar() {
 
   return (
     <NavbarLayout>
-      <Flex flexGrow={1} flexDir="column">
-        <Center>
-          <Box h="75px" my={4} position="relative" w="75%">
-            <Image
-              src="/cva_sinfondo.png"
-              layout="fill"
-              objectFit="scale-down"
-            />
-          </Box>
-        </Center>
-        <Stack spacing={2} px={2}>
-          {routes.map((route) => (
-            <NavbarItem {...route} key={route.route} />
-          ))}
-        </Stack>
-      </Flex>
-      {!loading && (
-        <Flex p={5} color="gray.400">
-          {user.firstName} {user.paternalName} {user.maternalName}
+      <Flex flexDir="column" h="100%">
+        <Flex flexGrow={1} flexDir="column">
+          <Center>
+            <Box h="75px" my={4} position="relative" w="75%">
+              <Image
+                src="/cva_sinfondo.png"
+                layout="fill"
+                objectFit="scale-down"
+              />
+            </Box>
+          </Center>
+          <Stack spacing={2} px={2}>
+            {routes.map((route) => (
+              <NavbarItem {...route} key={route.route} />
+            ))}
+          </Stack>
         </Flex>
-      )}
+        {!loading && (
+          <Flex p={5} color="gray.400">
+            {user.firstName} {user.paternalName} {user.maternalName}
+          </Flex>
+        )}
+      </Flex>
     </NavbarLayout>
   );
 }
