57b8681a3d205f8434acbbb03556007fab23c299
Arturo Torres <arturotc22@gmail.com>
2021-05-12 21:41:21 -0500

    Add guides

--- services/web/src/components/contents/GuidesIndexPageContent/index.tsx
+++ services/web/src/components/contents/GuidesIndexPageContent/index.tsx
@@ -1,29 +1,73 @@
 import React from "react";
 import { Box, SimpleGrid } from "@chakra-ui/react";
+import { useAuth } from "lib/hooks/useAuth";
+import { UserRoleName } from "lib/types/role";
 import { HeaderContent } from "./HeaderContent";
 import { DownloadCard } from "./DownloadCard";
 
 export function GuidesIndexPageContent() {
+  const { role } = useAuth();
+  const isSuperadmin = role === UserRoleName.SUPERADMIN;
+  const isTutor = role === UserRoleName.TUTOR;
+  const isSupervisor = role === UserRoleName.SUPERVISOR;
   return (
     <Box>
       <HeaderContent />
-      <Box mt={10}>
+      <Box my={10}>
         <SimpleGrid spacing={6} columns={[1, null, null, 2]}>
+          {(isSuperadmin || isTutor || isSupervisor) && (
+            <DownloadCard
+              title="Manual de Canvas para el tutor"
+              description="Archivo PDF"
+              filename="ManualDeCanvasParaElTutor.pdf"
+            />
+          )}
           <DownloadCard
-            title="Políticas de evaluación"
-            description="Las políticas de evaluación sirven para orientar a los tutores a que..."
-            filename="myfile.pdf"
+            title="Manual de Canvas para el participante"
+            description="Archivo PDF"
+            filename="ManualDeCanvasParaElParticipante.pdf"
+          />
+          {(isSuperadmin || isSupervisor || isTutor) && (
+            <DownloadCard
+              title="Cómo ser un buen profesor"
+              description="Archivo PDF"
+              filename="ComoSerUnBuenProfesor.pdf"
+            />
+          )}
+          <DownloadCard
+            title="Como descargar tu reconocimiento"
+            description="Archivo PDF"
+            filename="ComoDescargarTuReconocimiento.pdf"
           />
           <DownloadCard
-            title="Manual de como calificar"
-            description="Aquí mostraremos como calificar a un estudiante"
-            filename="grading.pdf"
+            title="Comparte tu experiencia"
+            description="Archivo PDF"
+            filename="ComparteTuExperiencia.pdf"
+          />
+          {(isSuperadmin || isTutor || isSupervisor) && (
+            <DownloadCard
+              title="Felicidades"
+              description="Archivo PDF"
+              filename="Felicidades-CentroVirtualDeAprendizaje.pdf"
+            />
+          )}
+          <DownloadCard
+            title="Calendario de aperturas"
+            description="Archivo PDF"
+            filename="CalendarioDeAperturas.pdf"
           />
           <DownloadCard
-            title="Horarios de atención"
-            description="Aquí mostramos todos los horarios de atención"
-            filename="hours.pdf"
+            title="Reporte de calificaciones"
+            description="Archivo Excel"
+            filename="ReporteDeCalificaciones.xlsx"
           />
+          {(isSuperadmin || isSupervisor) && (
+            <DownloadCard
+              title="Reporte de desempeño tutores"
+              description="Archivo Excel"
+              filename="ReporteDeDesempenoTutores.xlsx"
+            />
+          )}
         </SimpleGrid>
       </Box>
     </Box>
