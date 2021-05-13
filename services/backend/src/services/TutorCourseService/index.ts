55e0d7d04001497f4a5f20c7dd9e2b381cb8d8e9
Arturo Torres <arturotc22@gmail.com>
2021-05-13 00:13:24 -0500

    Add method to fetch courses of a tutor

--- services/backend/src/services/TutorCourseService/index.ts
+++ services/backend/src/services/TutorCourseService/index.ts
@@ -2,18 +2,23 @@ import { Service } from "typedi";
 import { Repository } from "typeorm";
 import { InjectRepository, Container } from "typeorm-typedi-extensions";
 import { ICreateArgs } from "./ICreateArgs";
+import { IFindCoursesForUser } from "./IFindCoursesForUser";
 import { TutorCourse } from "../../entities/TutorCourseEntity";
 import { UserService } from "../UserService";
 import { UserRoleName } from "../../entities/RoleEntity";
+import { Course } from "../../entities/CourseEntity";
+import { CourseService } from "../CourseService";
 
 @Service()
 export class TutorCourseService {
   private userService: UserService;
+  private courseService: CourseService;
   constructor(
     @InjectRepository(TutorCourse)
     private tutorCourseRepository: Repository<TutorCourse>
   ) {
     this.userService = Container.get(UserService);
+    this.courseService = Container.get(CourseService);
   }
 
   public async create(args: ICreateArgs): Promise<TutorCourse> {
@@ -27,5 +32,23 @@ export class TutorCourseService {
     const tutorCourse = new TutorCourse();
     Object.assign(tutorCourse, args);
     return this.tutorCourseRepository.save(tutorCourse);
+  }
+
+  private async findCoursesForUser({
+    userId,
+  }: IFindCoursesForUser): Promise<Course[]> {
+    const tutorCourses = await this.tutorCourseRepository.find({
+      tutorId: userId,
+    });
+    let courses: Course[] = [];
+    await tutorCourses.forEach(async (tutorCourse) => {
+      const course = await this.courseService.findOne({
+        claveCurso: tutorCourse.courseKey,
+      });
+      if (course) {
+        courses.push(course);
+      }
+    });
+    return courses;
   }
 }
