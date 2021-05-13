d6afa44fea6cce5e14578fc9d154e78e8b36be52
Arturo Torres <arturotc22@gmail.com>
2021-05-13 00:30:53 -0500

    Only superadmin see buttons to upload course

--- services/web/src/components/contents/CoursesPageContent/index.tsx
+++ services/web/src/components/contents/CoursesPageContent/index.tsx
@@ -1,4 +1,6 @@
-import { Box,
+import React, { useMemo, useState } from "react";
+import {
+  Box,
   useToast,
   Button,
   Flex,
@@ -10,21 +12,22 @@ import { Box,
   Stack,
   ModalBody,
   ModalFooter,
-  useDisclosure, } from "@chakra-ui/react";
+  useDisclosure,
+} from "@chakra-ui/react";
 import axios from "axios";
 import { backend } from "lib/constants/api";
 import { Course } from "lib/types/course";
-import { useMutation } from "react-query";
-import { CourseForm } from "./CourseForm";
-import React, { useMemo, useState } from "react";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { fetcherV1 } from "fetchers";
-import { CoursesTable } from "./CoursesTable";
-import { NoCoursesText } from "./NoCoursesText";
 import { ExcelUploaderSection } from "components/modules/ExcelUploaderSection";
-import { parseISO, format } from "date-fns";
+import { useAuth } from "lib/hooks/useAuth";
+import { UserRoleName } from "lib/types/role";
+import { CoursesTable } from "./CoursesTable";
+import { CourseForm } from "./CourseForm";
 
 export function CoursesPageContent() {
+  const { role } = useAuth();
+  const isSuperadmin = UserRoleName.SUPERADMIN === role;
   const toast = useToast();
   const [page, setPage] = useState(0);
   const [pageSize] = useState(20);
@@ -46,17 +49,27 @@ export function CoursesPageContent() {
   const { mutate } = useMutation<Course, Error, Course>((newCourse) =>
     axios.post(`${backend}/v1/courses`, newCourse, { withCredentials: true })
   );
-  const { isOpen: isOpenCreate, onOpen: onOpenCreate, onClose: onCloseCreate } = useDisclosure();
-  const { isOpen: isOpenUpload, onOpen: onOpenUpload, onClose: onCloseUpload } = useDisclosure();
+  const {
+    isOpen: isOpenCreate,
+    onOpen: onOpenCreate,
+    onClose: onCloseCreate,
+  } = useDisclosure();
+  const {
+    isOpen: isOpenUpload,
+    onOpen: onOpenUpload,
+    onClose: onCloseUpload,
+  } = useDisclosure();
 
   return (
     <Box>
       <Flex justifyContent="space-between" alignItems="center" mb={10}>
         <Heading fontSize="5xl">Cursos</Heading>
-        <Stack direction={"row"} spacing={2}>
-          <Button onClick={onOpenCreate}>Crear Curso</Button>
-          <Button onClick={onOpenUpload}>Subir Cursos</Button>
-        </Stack>
+        {isSuperadmin && (
+          <Stack direction="row" spacing={2}>
+            <Button onClick={onOpenCreate}>Crear Curso</Button>
+            <Button onClick={onOpenUpload}>Subir Cursos</Button>
+          </Stack>
+        )}
       </Flex>
       {isLoading && <Spinner />}
       {tableData !== undefined && <CoursesTable data={tableData.courses} />}
@@ -105,10 +118,18 @@ export function CoursesPageContent() {
                 recognitionType: "",
                 url: "",
                 startDate: "",
-                endDate: ""
+                endDate: "",
               }}
               onSubmit={(
-                { name, topic, duration, recognitionType, url, startDate, endDate },
+                {
+                  name,
+                  topic,
+                  duration,
+                  recognitionType,
+                  url,
+                  startDate,
+                  endDate,
+                },
                 actions
               ) => {
                 mutate(
@@ -119,7 +140,7 @@ export function CoursesPageContent() {
                     recognitionType,
                     url,
                     startDate,
-                    endDate
+                    endDate,
                   },
                   {
                     onSuccess: () => {
