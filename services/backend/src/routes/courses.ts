4b8c82c1c7b3c2eb9aaba815d4ab6e1461cec676
Arturo Torres <arturotc22@gmail.com>
2021-05-16 20:09:29 -0500

    Protect routes

--- services/backend/src/routes/courses.ts
+++ services/backend/src/routes/courses.ts
@@ -2,11 +2,13 @@ import CourseController from "../controllers/CourseController";
 import { IRoute } from "../lib/IRoute";
 import { Route } from "../lib/Route";
 import { ExcelFileUpload } from "../middleware/ExcelFileUpload";
+import { RouteAuthRolesEnum } from "../lib/RouteAuthRolesEnum";
 
 export const routes: IRoute[] = [
   new Route({
     path: "/courses",
     method: "GET",
+    requireAuth: [RouteAuthRolesEnum.SUPERADMIN, RouteAuthRolesEnum.SUPERVISOR],
     withController: {
       action: "courses",
       controller: CourseController,
@@ -15,6 +17,7 @@ export const routes: IRoute[] = [
   new Route({
     path: "/courses",
     method: "POST",
+    requireAuth: [RouteAuthRolesEnum.SUPERADMIN],
     withController: {
       action: "create",
       controller: CourseController,
