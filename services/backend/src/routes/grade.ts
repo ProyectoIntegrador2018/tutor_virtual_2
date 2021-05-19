d8fc7ddd3adc4c634b42b179712abf4ab9f96924
hectordeluna <hectordelunap@gmail.com>
2021-05-19 13:22:30 -0500

    Update grade.ts

--- services/backend/src/routes/grade.ts
+++ services/backend/src/routes/grade.ts
@@ -2,6 +2,7 @@ import { ExcelFileUpload } from "../middleware/ExcelFileUpload";
 import GradeController from "../controllers/GradeController";
 import { IRoute } from "../lib/IRoute";
 import { Route } from "../lib/Route";
+import { RouteAuthRolesEnum } from "../lib/RouteAuthRolesEnum";
 
 export const routes: IRoute[] = [
   new Route({
@@ -15,6 +16,7 @@ export const routes: IRoute[] = [
   new Route({
     path: "/grades/load-from-excel",
     method: "POST",
+    requireAuth: [RouteAuthRolesEnum.TUTOR],
     middleware: [ExcelFileUpload()],
     withController: {
       action: "uploadGrades",
